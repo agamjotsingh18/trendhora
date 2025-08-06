661b2e7eab26f581532d6ce125069f5b9b95c401
priyam <ghoshpriyam191@gmail.com>
2025-08-06 11:08:14 +0530

    layout fixed

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false); // Add this line
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/test");
         console.log("Mongo DB Connected: ", conn.connection.host);
     } catch (err) {
         console.log(err);
