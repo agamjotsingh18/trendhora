c7976cd3b275579c7873ca5a959f12121c86f710
Samridhi <samridhiprakash27@gmail.com>
2025-09-29 19:40:09 +0530

    updated-ui-product-card

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,40 +1,40 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import Loader from "../components/Loader/loader.js";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Home from '../routes/Home';
-import About from '../routes/About';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
-import ManageAccount from '../components/Account/ManageAccount/ManageAccount';
-import MyAccount from '../components/Account/MyAccount/MyAccount';
-import Shop from '../components/Shop/Shop';
-import ItemView from '../routes/ItemView';
-import CategoryView from '../routes/CategoryView';
-import SearchView from '../routes/Search';
-import CartItemsProvider from '../Context/CartItemsProvider';
-import Login from '../components/Authentication/Login/Login';
-import Register from '../components/Authentication/Register/Register';
-import Wishlist from '../components/Wishlist';
-import WishItemsProvider from '../Context/WishItemsProvider';
+import Home from "../routes/Home";
+import About from "../routes/About";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import ManageAccount from "../components/Account/ManageAccount/ManageAccount";
+import MyAccount from "../components/Account/MyAccount/MyAccount";
+import Shop from "../components/Shop/Shop";
+import ItemView from "../routes/ItemView";
+import CategoryView from "../routes/CategoryView";
+import SearchView from "../routes/Search";
+import CartItemsProvider from "../Context/CartItemsProvider";
+import Login from "../components/Authentication/Login/Login";
+import Register from "../components/Authentication/Register/Register";
+import Wishlist from "../components/Wishlist";
+import WishItemsProvider from "../Context/WishItemsProvider";
 // import Checkout from '../components/Checkout/Checkout';
-import SearchProvider from '../Context/SearchProvider';
-import Toaster from '../components/Toaster/toaster';
-import { ThemeProvider } from '../Context/ThemeContext';
+import SearchProvider from "../Context/SearchProvider";
+import Toaster from "../components/Toaster/toaster";
+import { ThemeProvider } from "../Context/ThemeContext";
 
-import FaqList from '../Pages/Footer/Faq/FaqList.js';
-import AccessiblityPage from '../Pages/Footer/Accessibility/Accessibility.js';
-import RefundPage from '../Pages/Footer/Refund/Refund.js';
-import ShippingPage from '../Pages/Footer/Shipping/Shipping.js';
-import TermsConditions from '../components/Legal/TermsConditions/TermsConditions';
-import PrivacyPolicy from '../components/Legal/PrivacyPolicy/PrivacyPolicy';
-import ContactUs from '../routes/ContactUs';
+import FaqList from "../Pages/Footer/Faq/FaqList.js";
+import AccessiblityPage from "../Pages/Footer/Accessibility/Accessibility.js";
+import RefundPage from "../Pages/Footer/Refund/Refund.js";
+import ShippingPage from "../Pages/Footer/Shipping/Shipping.js";
+import TermsConditions from "../components/Legal/TermsConditions/TermsConditions";
+import PrivacyPolicy from "../components/Legal/PrivacyPolicy/PrivacyPolicy";
+import ContactUs from "../routes/ContactUs";
 
 // ✅ Import RecentlyViewedSection
-import RecentlyViewedSection from '../components/RecentlyViewedSection';
+import RecentlyViewedSection from "../components/RecentlyViewedSection";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -84,6 +84,7 @@ function App() {
                   <Route path="*" element={<Login />} />
                 </Route>
                 <Route path="/shop" element={<Shop />} />
+
                 <Route path="/category">
                   <Route path=":id" element={<CategoryView />} />
                 </Route>
